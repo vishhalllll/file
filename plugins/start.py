ced4f066bcc0c9393699f55ee914339cf96bcb18
vishhalllll <108014711+vishhalllll@users.noreply.github.com>
2025-12-19 23:08:57 +0530

    Configure MIN_CHANNEL_ID in start.py
    Set MIN_CHANNEL_ID for Pyrogram to a specific value.

--- plugins/start.py
+++ plugins/start.py
@@ -12,6 +12,8 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed,decode, get_messages, delete_file
 from database.database import add_user, del_user, full_userbase, present_user
 
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1002665884135
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
